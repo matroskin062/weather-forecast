1674544a7cc98e1a2ab30b17709e5ca54ef606da
matroskin062 <dima.matroskin062@gmail.com>
2020-07-22 17:21:10 +0300

    added https to api url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import * as Axios from 'axios';
 
 const API_KEY = '166f41f09442b35e807cb8bce5b3cfe6';
 export const axios = Axios.create({
-  baseURL: `http://api.openweathermap.org/data/2.5/`,
+  baseURL: `https://api.openweathermap.org/data/2.5/`,
 });
 
 axios.interceptors.request.use((config) => {
