3348108bc95691f5d2cb90230d4286a3acc8b7cd
matroskin062 <dima.matroskin062@gmail.com>
2020-07-23 17:10:44 +0300

    location function little refactoring

--- src/App.js
+++ src/App.js
@@ -5,25 +5,36 @@ import Weather from './components/Weather/Weather';
 import SearchCity from './components/SearchCity/SearchCity';
 import { fetchForecast } from './redux/actions/forecast';
 import Forecast from './components/Forecast/Forecast';
+import Axios from 'axios';
 
 function App() {
   const dispatch = useDispatch();
   const weatherData = useSelector(({ weather }) => weather.data);
   const forecastData = useSelector(({ forecast }) => forecast.data);
+  const [error, setError] = React.useState(false);
+  const locationSuccess = ({ coords }) => {
+    const params = {
+      lat: coords.latitude,
+      lon: coords.longitude,
+    };
+    dispatch(fetchWeatherData(params));
+    dispatch(fetchForecast(params));
+  };
+
   React.useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords }) => {
-      const params = {
-        lat: coords.latitude,
-        lon: coords.longitude,
-      };
-      dispatch(fetchWeatherData(params));
-      dispatch(fetchForecast(params));
-    });
-  }, [dispatch]);
+    navigator.geolocation.getCurrentPosition(
+      locationSuccess,
+      () => setError(true),
+      { enableHighAccuracy: true }
+    );
+  }, []);
 
   return (
     <div className='weather-app'>
       <SearchCity />
+      {!weatherData && error ? (
+        <h1>Невозомжно определить местоположение: </h1>
+      ) : null}
       {weatherData && (
         <>
           <h1 className='title'>{weatherData.name}</h1>
